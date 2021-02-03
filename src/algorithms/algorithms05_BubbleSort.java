ab94f28605c563aa48b6c42f74bd46160981bd80
Ilyas <fortmayn@mail.ru>
2021-02-03 23:44:47 +0300

    added bubble sort, insertion sort, selection search

--- src/algorithms/algorithms05_BubbleSort.java
+++ src/algorithms/algorithms05_BubbleSort.java
@@ -1,2 +1,70 @@
-package algorithms;public class algorithms05_BubbleSort {
+package algorithms;
+/*
+Bubble Sort is the simplest sorting algorithm
+that works by repeatedly swapping the adjacent
+elements if they are in wrong order.
+Example:
+First Pass:
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm
+compares the first two elements, and swaps since
+5 > 1.
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these
+elements are already in order (8 > 5), algorithm
+does not swap them.
+
+Second Pass:
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
+( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 )
+Now, the array is already sorted, but our algorithm
+does not know if it is completed. The algorithm needs
+one whole pass without any swap to know it is sorted.
+
+Third Pass:
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+
+The above function always runs O(n^2) time even if the
+array is sorted. It can be optimized by stopping the
+algorithm if inner loop didn’t cause any swap.
+ */
+public class algorithms05_BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        System.out.println("Unsorted array is:");
+        printArray(arr);
+        bubbleSort(arr, arr.length);
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int[] arr, int n){
+        boolean swapped;
+        for(int i = 0; i < n; i++) {
+            swapped = false;
+            for(int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped == false) {
+                break;
+            }
+        }
+    }
 }
--- src/algorithms/algorithms06_InsertionSort.java
+++ src/algorithms/algorithms06_InsertionSort.java
@@ -1,9 +1,54 @@
 package algorithms;
+/*
+Insertion sort is a simple sorting algorithm
+that works similar to the way you sort playing
+cards in your hands. The array is virtually
+split into a sorted and an unsorted part.
+Values from the unsorted part are picked and
+placed at the correct position in the sorted part.
+Algorithm
+To sort an array of size n in ascending order:
+1: Iterate from arr[1] to arr[n] over the array.
+2: Compare the current element (key) to its predecessor.
+3: If the key element is smaller than its predecessor,
+compare it to the elements before. Move the greater
+elements one position up to make space for the
+swapped element.
+Time Complexity: O(n*2)
+Auxiliary Space: O(1)
+ */
 
-
-// time complexity O(Math.sqrt(n^2))
-class algorithms06_InserionSort {
+public class algorithms06_InsertionSort {
     public static void main(String[] args) {
-        
+        int arr[] = {12, 11, 13, 5, 6 };
+        System.out.println("Unsorted array:");
+        printArray(arr);
+        insertionSort(arr);
+        System.out.println("Sorted array: ");
+        printArray(arr);
+
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void insertionSort(int arr[]){
+        int n = arr.length;
+        for(int i = 1; i < n; i++){
+            int key = arr[i];
+            int j = i - 1;
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
