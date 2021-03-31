e5459a3361c579076f943966ce05faaae221fb65
Ilyas <fortmayn@mail.ru>
2021-03-31 22:26:06 +0300

    added quicksort

--- src/collection/Collection06_Iterable.java
+++ src/collection/Collection06_Iterable.java
@@ -1,2 +1,6 @@
 package collection;public class Collection06_Iterable {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        int _cat$6 = 12;
+    }
 }
--- src/test.java
+++ src/test.java
@@ -1,20 +1,12 @@
+import java.util.*;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        int sum = 0;
-        for(int i = 0; i < 366;i++) {
-            sum += i;
-        }
-        System.out.println(sum);
-        try {
-            Thread.sleep(500);
-        } catch (Exception e) {
-            System.out.println("something goes wrong");
-        }
-        System.out.println(start - System.currentTimeMillis());
+
+
+        String s = "peterburg";
+        System.out.println(s.endsWith("burg"));
+
     }
-
-
 }
